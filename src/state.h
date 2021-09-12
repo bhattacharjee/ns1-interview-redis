123a1ed0a4f8752ce6010f364c631c203e5fddc7
Rajbir Bhattacharjee <rajbir.bhattacharjee@gmail.com>
2021-09-12 06:00:11 -0700

    checkpoint

--- src/state.h
+++ src/state.h
@@ -3,6 +3,7 @@
 
 #include "common_include.h"
 #include "resp_parser.h"
+#include <cstring>
 
 typedef enum
 {
@@ -22,14 +23,21 @@ struct State
     StateState                              m_state;
     std::string                             m_read_data;
     std::shared_ptr<AbstractRespObject>     m_resp_object;
+    std::shared_ptr<AbstractRespObject>     m_response;
     int                                     m_socket;
     mutable std::mutex                      m_mutex;
+
+    // In case of some unrecoverable error, this will
+    // be used to communicate the error to the client
+    char m_special_error[64];
 
     State(int fd)
     {
         m_resp_object = std::shared_ptr<AbstractRespObject>(nullptr);
+        m_response = std::shared_ptr<AbstractRespObject>(nullptr);
         m_state = STATE_INVALID;
         m_socket = fd;
+        m_special_error[0] = 0;
     }
 
     static std::shared_ptr<State>
@@ -37,6 +45,16 @@ struct State
     {
         auto p = new (std::nothrow) State(fd);
         return std::shared_ptr<State>(p);
+    }
+
+    void set_special_error(const char* err)
+    {
+        strncpy(m_special_error, err, 63);
+    }
+
+    void set_default_special_error()
+    {
+        set_special_error("-Unexpected Error\r\n");
     }
 };
 
